2e0d5ba9e2c8dccc78f864535775ede6f8657c51
Nelson Wells <nelsonandrewwells@gmail.com>
2017-03-13 12:43:29 -0700

    Add rice question logic for checkboxes.

--- app/src/main/java/com/japanquiz/android/japanquiz/MainActivity.java
+++ app/src/main/java/com/japanquiz/android/japanquiz/MainActivity.java
@@ -14,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     public int quizScore = 0;
 
+    public int riceCheckBoxCount = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -96,14 +99,77 @@ public class MainActivity extends AppCompatActivity {
                 if (checked)
                     tv.setText(R.string.tanuki_bit);
                 tv.setVisibility(View.VISIBLE);
+                quizScore += 1;
                 break;
         }
         ImageView tanukiImage = (ImageView) findViewById(R.id.tanuki_image);
         tanukiImage.setVisibility(View.VISIBLE);
     }
 
-//    public void onRiceFoodsCheckBoxesChecked(View v) {
-//        boolean checked = ((CheckBox) v).isChecked();
-//
-//    }
+    public void onRiceFoodsCheckBoxesChecked(View v) {
+        boolean checked = ((CheckBox) v).isChecked();
+        TextView tv = (TextView) findViewById(R.id.rice_set_answer_text);
+
+        switch (v.getId()) {
+            case R.id.onigiri_checkbox:
+                if (checked)
+                    tv.setText(R.string.onigiri_bit);
+                riceCheckBoxCount += 1;
+                if (riceCheckBoxCount == 3) {
+                    quizScore += 1;
+                    tv.setText(R.string.rice_answer_set);
+                }
+                break;
+            case R.id.mochi_checkbox:
+                if (checked)
+                    tv.setText(R.string.mochi_bit);
+                riceCheckBoxCount += 1;
+                if (riceCheckBoxCount == 3) {
+                    tv.setText(R.string.rice_answer_set);
+                    quizScore += 1;
+                }
+                break;
+            case R.id.sake_checkbox:
+                if (checked)
+                    tv.setText(R.string.sake_bit);
+                riceCheckBoxCount += 1;
+                if (riceCheckBoxCount == 3) {
+                    tv.setText(R.string.rice_answer_set);
+                    quizScore += 1;
+                }
+                break;
+            case R.id.tamago_checkbox:
+                if (checked)
+                    tv.setText(R.string.ramen_bit);
+                break;
+            case R.id.chashu_checkbox:
+                if (checked)
+                    tv.setText(R.string.chashu_bit);
+                break;
+            case R.id.ramen_checkbox:
+                if (checked)
+                    tv.setText(R.string.ramen_bit);
+                break;
+        }
+
+    }
+
+
+    public void onMisogiQuestionChosen(View v) {
+        boolean chosen = ((RadioButton) v).isChecked();
+        TextView tv = (TextView) findViewById(R.id.misogi_answer_set_text);
+
+        switch (v.getId()) {
+            case R.id.misogi_true:
+                if (chosen) {
+                    tv.setText(R.string.misogi_answer_right);
+                    quizScore += 1;
+                    break;
+                }
+                if (chosen) {
+                    tv.setText(R.string.misogi_answer_wrong);
+                    break;
+                }
+        }
+    }
 }
